43a133251a464c1b04ceb3cd5b0cba3c712bf79a
Jacksopop <75774890+Jacksopop@users.noreply.github.com>
2024-11-02 14:00:25 -0700

    Landing

--- src/Landing.js
+++ src/Landing.js
@@ -1,6 +1,20 @@
 import React from "react";
-const Landing =() => <h1>landing page
-</h1>;
+const Landing = () => (
+    <div>
+      <h1 style ={{ textAlign: 'center',fontFamily: 'Arial'}}>ZotPlanner</h1>
+      <div
+        style={{
+          width: 200,
+          height: 100,
+          backgroundColor: 'lightblue',
+          border: '1px solid ',
+          color:'whitesmoke'
+        }}
+      >
+        Button
+      </div>
+    </div>
+  );
 export default Landing;
 
 

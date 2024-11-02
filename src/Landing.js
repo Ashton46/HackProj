2e7ee58ab36a247717e2884edba852739d962867
Jacksopop <75774890+Jacksopop@users.noreply.github.com>
2024-11-02 16:36:18 -0700

    3 boxes

--- src/Landing.js
+++ src/Landing.js
@@ -1,20 +1,35 @@
 import React from "react";
-const Landing = () => (
-    <div>
-      <h1 style ={{ textAlign: 'center',fontFamily: 'Arial'}}>ZotPlanner</h1>
-      <div
-        style={{
-          width: 200,
-          height: 100,
-          backgroundColor: 'lightblue',
-          border: '1px solid ',
-          color:'whitesmoke'
-        }}
-      >
-        Button
-      </div>
-    </div>
+import './Landing.css';
+
+
+function Landing() {
+   // <div className="BackGround">
+     //   <h1 className = "Title">
+       //     ZotPlanner</h1>
+        //<div className = "Calendar">
+          //  hello
+        //</div>
+    //</div>
+  
+  return(
+
+    <div className="BackGround">
+        <h1 className = "Title">
+           ZotPlanner</h1>
+
+    <button className = "Box1" type="button" onClick="window.location.href='/calendar;">
+    Box2
+  </button>
+  <button className = "Box2" type="button" onClick={() =>window.location.href = 'http://localhost:3002/todo'}>
+    Box1
+  </button>
+  <button className = "Box3" type="button" onClick="window.location.href='/calendar;">
+    Box 3
+  </button>
+  </div>
   );
+}
+  
 export default Landing;
 
 
